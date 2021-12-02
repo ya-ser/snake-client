3a73c8fb4eabe27793199689fcb6e4f46fe4a160
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2021-12-02 05:21:39 +0000

    name is now displaying on snake

--- client.js
+++ client.js
@@ -10,7 +10,7 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
-    console.log(`Yaser has connected!`);
+    conn.write("Name: YAS")
   });
 
   conn.on('data', (data) => {
