706b5dd86aa70011de83425ea72eaa5e93d8dfe8
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2021-12-02 06:04:36 +0000

    created input file to have both setupInput and handleUserInput functions from play file.

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
-const {connect} = require("./client");//
-
-// const clientMod = require("./client");
-// console.log("clientMod: ", clientMod);
-// const connect = clientMod.connect;
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
+const {connect} = require("./client");
+const {setupInput} = require("./input");
 
 console.log("Connecting ...");
 connect();
+setupInput();
