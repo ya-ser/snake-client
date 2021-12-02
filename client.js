40a58510c00427592b4b218a70fbba5f35fcbd9d
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2021-12-02 07:28:23 +0000

    refactored client to remove hard coded data and added it to constants file

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require("net");
+const {IP, PORT} = require("./constants");
 
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',// IP address here,
-    port: 50541// PORT number here,
+    host: IP,// IP address here,
+    port: PORT// PORT number here,
   });
 
   // interpret incoming data as text
