6f53d92b0d4e196948ef74ca0f2ac02fc897ff39
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2021-12-02 06:23:01 +0000

    created wasd movements

--- input.js
+++ input.js
@@ -1,4 +1,12 @@
-const setupInput = function () {
+let connection;
+
+let w = "Move: up"
+let a = "Move: left"
+let s = "Move: down"
+let d = "Move: right"
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,6 +18,14 @@ const setupInput = function () {
 const handleUserInput = function (key) {
   if (key === "\u0003") {
     process.exit();
+  } else if (key === 'w') {
+    connection.write(w);
+  } else if (key === 'a') {
+    connection.write(a);
+  } else if (key === 's') {
+    connection.write(s);
+  } else if (key === 'd') {
+    connection.write(d);
   }
 };
 
